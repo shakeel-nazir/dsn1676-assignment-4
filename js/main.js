e380790c590b168b22853dc73fa17f4f967239a0
shakeel-nazir <shakeelnazir@MacBook-Pro.local>
2015-02-12 15:38:55 -0500

    Started Site
    - Started Site

---
+++ js/main.js
@@ -0,0 +1,31 @@
+//alert("Hello!");
+
+
+// String: just some text. always inside qoutes
+var firstName = "Shakeel";
+console.log(firstName);
+
+
+
+//Numbers: can mathified, not in quotes
+var age = 32;
+
+// boolean: true or false 
+var isHuman = true;
+
+
+// Array: a box that contains many things 
+var dinos = ["Igunadon", "Pteranodon", "T-Rex"];
+console.log(dinos[1]);
+
+
+//Object: avariable that contains other variables 
+var trex = {
+    fullName: "Tyrannosauris Rex",
+    age: 600000000,
+    isMeatEater: true
+};
+
+console.log(trex.fullName);
+
+
