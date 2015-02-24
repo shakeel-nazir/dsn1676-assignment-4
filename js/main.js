d3c575e2c15bc7ff4dc0a0e091321339fb3bfbb4
shakeel-nazir <shakeelnazir@MacBook-Pro.local>
2015-02-23 23:08:35 -0500

    Spacing in Code Issue
    - Spacing in Code Issue

--- js/main.js
+++ js/main.js
@@ -1,33 +1,50 @@
-//Object: avariable that contains other variables 
 var blueberries = {
+   
     fruit: "Blueberries",
+    
     energy: 240,
+    
     carbohydrates: 14.49,
+    
     grows: true,
+    
     latio:"Vaccinium Corymbosum",
+    
     protein: 0.74
+
 };
 
 var grapes = {
+    
     fruit: "Grapes",
+    
     energy: 288,
+    
     carbohydrates: 18.1,
+    
     grows: false,
+    
     latio:"Vitis Vinifera",
+    
     protein: 0.72
-};
 
+};
 
 var redCurrant = {
+    
     fruit: "Red Currant",
+    
     energy: 234,
+    
     carbohydrates: 13.8,
+    
     grows: true,
+    
     latio:"Ribes Rubrum",
+    
     protein: 1.4
+
 };
-
-
 
 var writeFruit = function (fruits) { 
     
@@ -61,6 +78,3 @@ var writeFruit = function (fruits) {
 
 var allFruit = [blueberries, grapes, redCurrant];
 allFruit.forEach(writeFruit)
-
-
-
